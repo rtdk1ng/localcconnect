85799132606b9b3c409b3cbd319d2fedac8caed0
Волохин Андрей Сергеевич <@corp.itstep>
2021-01-27 19:25:46 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,194 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.bundle.min.js" integrity="sha384-BOsAfwzjNJHrJ8cZidOg56tcQWfp6y72vEJ8xQ9w6Quywb24iOsW913URv1IS4GD" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/fontawesome.css">
+</head>
+<body>
+    <header>
+        <div class="header_width">
+            <div class="header_wrap">
+                <div class="header__section">
+                    <div class="header__logo">
+                        <i class="fas fa-walking"> LC</i>
+                    </div>
+                </div>
+                <nav>
+                <div class="header__section">
+                    <div class="header__item">
+                        <a href="index.html"><span class="underline">Главная</span></a>
+                    </div>
+                    <div class="header__item">
+                        <a href="index2.html"><span class="underline">Услуги</span></a>
+                    </div>
+                    <div class="header__item">
+                        <a href="index3.html"><span class="underline">Контакты</span></a>
+                    </div>
+                    <div class="header__item">
+                        <a href="index4.html"><span class="underline">О нас</span></a>
+                    </div>
+                    <div class="header__item">
+                        <a href="index5.html"><span class="underline">Жалоба</span></a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+<div class="container image">
+    <div class="container inside">
+        <div class="container-wrap">
+            <div class="container-text">
+                <h2 class="animate__animated animate__backInLeft"> Срочный ремонт
+                        компьютерной техники с 
+                        выездом на дом или в офис
+                </h2>
+            </div>
+        </div>
+        <div class="container-wrap">
+            <div class="form animate__animated animate__backInRight">
+                <form action="">
+                    <h4>Оставьте заявку</h4>
+                    <p>Наши специалисты свяжутся с вами в течение 15 минут</p>
+                    <input type="text" placeholder="Имя">
+                    <input type="text" placeholder="Номер телефона">
+                    <input type="text" placeholder="Адрес">
+                    <button class="form_button">Оставить заявку</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Container -->
+<div class="container hww">
+    <div class="row wrap">
+        <div class="mtop">
+            <h2>Как мы работаем?</h2>
+        </div>
+        <div class="col-md-3 numbers">
+            <div class="detection">
+                <h2>1</h2>
+            </div>
+        </div>
+        <div class="col-md-3 numbers">
+            <div class="detection">
+                <h2>2</h2>
+            </div>
+        </div>
+        <div class="col-md-3 numbers">
+            <div class="detection">
+                <h2>3</h2>
+            </div>
+        </div>
+        <div class="col-md-3 numbers">
+            <div class="detection">
+                <h2>4</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row wrap">
+        <div class="col-md-3">
+            <p>Указывайте удобное для вас время. Звоните по тел или воспользуйтесь формой онлайн заказа.</p>
+        </div>
+        <div class="col-md-3">
+            <p>Проводит диагностику и определяет неисправность.
+                    Сообщает стоимость работ.</p>
+        </div>
+        <div class="col-md-3">
+            <p>Согласовав с вами предстоящий ремонт, устранит неисправность и даст рекомендации.</p>
+        </div>
+        <div class="col-md-3 bordernone">
+            <p>В конце работ мастер оставляет вам договор и отчет. Обращайтесь с этими документами, если возникнет гарантийный случай.</p>
+        </div>
+    </div>
+</div> 
+<!-- Container Discussion -->
+<div class="container discussion">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="discussion_image">
+                <img src="img/discussion2.png" alt="#">
+            </div>
+        </div>
+        <div class="col-md-6 margin-col">
+            <div class="discussion_text">
+                <h3>
+                    Услуги для компаний
+                </h3>
+            </div>
+            <div class="discussion_text">
+                <p>Наша компания предоставляет услуги по обслуживания IT-Инфраструктур по самым выгодным ценам.</p>
+            </div>
+            <div class="discussion_text">
+                <ol>
+                    <li>Обслуживания компьютеров и оргтехники</li>
+                    <li>Обслуживания локальных сетей</li>
+                    <li>Обслуживания систем безопасности
+                        (CCTV, СКУД и ОПС)</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Container Reviews -->
+<div class="container reviews">
+    <div class="mtop">
+        <h2>Отзывы</h2>
+    </div>
+    <div class="row wrap">
+        <div class="col-md-3 silver">
+            <h2>Тимур</h2>
+            <p>Ваш сервис мне посоветовал коллега по работе.
+                        Спасибо большое вам за чистку ноутбука, он очень сильно грелся и шумел, сделал все очень быстро, теперь если вдруг что, пойду к вам и друзьям советовать буду Ещё раз спасибо большое</p>
+        </div>
+        <div class="col-md-3 silver">
+            <h2>Анна</h2>
+            <p>Спасибо большое за помощь!
+                        Во время транспортировки разбил экран моего аймака, так как это был рабочий аппарат не было времени долго ждать, загуглил сервисные центры и позвонил сюда, мастер приехал оперативно и забрал технику в сервис, на следующий день я уже получил его с новым экраном</p>
+        </div>
+        <div class="col-md-3 silver">
+            <h2>Айсулу</h2>
+            <p>Компьютер зависал намертво с синим экраном, вызвал мастера, проблема оказалась связана с оперативной памятью, мастер прямо на месте поменял на новую и проблема исчезла. Спасибо за оперативную помощь.</p>
+        </div>
+        <div class="col-md-3 bordernone silver">
+            <h2>Роман</h2>
+            <p>Специалист компании быстро отреагировал на мою проблему. Компьютер в один момент перестал включаться, мастер в сервисе сказал, что сломался блок питания, и посоветовал заменить его, так как чинить его выйдет дороже. Так и поступили, времени много это не заняло и по деньгам вышло не дорого.</p>
+        </div>
+    </div>
+</div>
+<!-- Footer -->
+<footer class="footer"> 
+    <div class="container footer_container">
+        <div class="row row_footer">
+            <div class="col-md-6">
+                <div class="links">
+                    <p>© All Right Reserved. LC co. +7 747 747 47 47</p>
+                </div>
+            </div>
+            <div class="col-md-6 footer_col">
+                <div class="links_icon">
+                    <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://vk.com/"><i class="fab fa-vk"></i></a>
+                    <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+    <script src="https://kit.fontawesome.com/a12d129292.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js" integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous"></script>
+</body>
+</html>
